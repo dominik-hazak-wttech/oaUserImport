29c258fd3587f380131fc40f07d0268ecdb3cb4e
Dominik Hazak <dominik.hazak@vml.com>
2025-05-27 12:49:41 +0200

    Final code - removed validation only

--- Functions/20-BookingImport.ps1
+++ Functions/20-BookingImport.ps1
@@ -12,14 +12,13 @@ if($dec.ToLower() -ne "y"){
 $bookingTypes = ($connector.SendRequest([OARequestType]::Read,@{type="BookingType"; method="all"; queryData=@{}; limit=100},$false)).response.Read.BookingType
 
 $importToSandbox = $true
+$validateOnly = $false
 
 Write-Host "Checking users in OpenAir"
 . ./Functions/20.1-CheckAllUsers.ps1
 
 Write-Host "Checking projects in OpenAir"
 . ./Functions/20.2-CheckAllProjects.ps1
-
-$validateOnly = $true
 
 Write-Host "Importing Bookings to OpenAir"
 . ./Functions/20.3-ImportBookings.ps1
